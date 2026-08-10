df5eb326fd614e871186bb6ae40985022b83635f
MounikaPedalenka <mounikapedalenka273@gmail.com>
2026-08-10 16:43:58 +0530

    deploy commit

--- server/server.js
+++ server/server.js
@@ -16,7 +16,17 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://hirings-xi.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
