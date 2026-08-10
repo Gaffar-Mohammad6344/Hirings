90efc5c770052568c6d5a93a94dd5f367ed3d76a
MounikaPedalenka <mounikapedalenka273@gmail.com>
2026-08-10 22:57:40 +0530

    semi contactform

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -198,7 +198,10 @@ export default function ContactForm() {
     setStatus({ type: '', message: '' });
 
     try {
-      const response = await axios.post('http://localhost:5000/api/contact', formData);
+   const response = await axios.post(
+  `${import.meta.env.CLIENT_URL}/api/contact`,
+  formData
+);
       if (response.data.success) {
         setStatus({ type: 'success', message: 'Message sent! We will get back to you shortly.' });
         
