b540cad1ebaf756196f61e1d3c972d0f5e208216
MounikaPedalenka <mounikapedalenka273@gmail.com>
2026-08-11 22:32:00 +0530

    local

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -116,37 +116,6 @@
 
 
 
-// import { Resend } from "resend";
-
-// const resend = new Resend(process.env.RESEND_API_KEY);
-
-// const sendEmail = async ({ email, subject, html, replyTo }) => {
-//   try {
-//     const { data, error } = await resend.emails.send({
-//       from: "Hirings <onboarding@resend.dev>",
-//       to: [email],
-//       replyTo: replyTo,
-//       subject: subject,
-//       html: html,
-//     });
-
-//     if (error) {
-//       console.error("Resend Error:", error);
-//       throw new Error(error.message || "Email sending failed");
-//     }
-
-//     console.log("Email sent successfully:", data?.id);
-
-//     return data;
-//   } catch (error) {
-//     console.error("Resend Email Error:", error);
-//     throw new Error("Email sending failed");
-//   }
-// };
-
-// export default sendEmail;
-
-
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -156,9 +125,9 @@ const sendEmail = async ({ email, subject, html, replyTo }) => {
     const { data, error } = await resend.emails.send({
       from: "Hirings <onboarding@resend.dev>",
       to: [email],
-      replyTo,
-      subject,
-      html,
+      replyTo: replyTo,
+      subject: subject,
+      html: html,
     });
 
     if (error) {
@@ -171,7 +140,7 @@ const sendEmail = async ({ email, subject, html, replyTo }) => {
     return data;
   } catch (error) {
     console.error("Resend Email Error:", error);
-    throw error;
+    throw new Error("Email sending failed");
   }
 };
 
