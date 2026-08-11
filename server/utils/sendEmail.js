9edba9843a630aff9e95c7f97c3cd23e3f0c261f
MounikaPedalenka <mounikapedalenka273@gmail.com>
2026-08-11 23:07:07 +0530

    locsss

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -116,40 +116,6 @@
 
 
 
-// import { Resend } from "resend";
-
-// const resend = new Resend(process.env.RESEND_API_KEY);
-
-// const sendEmail = async ({ email, subject, html, replyTo }) => {
-//   try {
-//     const { data, error } = await resend.emails.send({
-//       from: "Hirings <onboarding@resend.dev>",
-//       to: [email],
-//       replyTo: replyTo,
-//       subject: subject,
-//       html: html,
-//     });
-
-//     if (error) {
-//       console.error("Resend Error:", error);
-//       throw new Error(error.message || "Email sending failed");
-//     }
-
-//     console.log("Email sent successfully:", data?.id);
-
-//     return data;
-//   } catch (error) {
-//     console.error("Resend Email Error:", error);
-//     throw new Error("Email sending failed");
-//   }
-// };
-
-// export default sendEmail;
-
-
-
-
-
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -157,24 +123,24 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ email, subject, html, replyTo }) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: "Hirings <info@hirings.com>",
+      from: "Hirings <onboarding@resend.dev>",
       to: [email],
-      replyTo,
-      subject,
-      html,
+      replyTo: replyTo,
+      subject: subject,
+      html: html,
     });
 
     if (error) {
-      console.error("RESEND ERROR:", error);
-      throw error;
+      console.error("Resend Error:", error);
+      throw new Error(error.message || "Email sending failed");
     }
 
-    console.log("EMAIL SENT:", data?.id);
+    console.log("Email sent successfully:", data?.id);
 
     return data;
   } catch (error) {
-    console.error("SEND EMAIL ERROR:", error);
-    throw error;
+    console.error("Resend Email Error:", error);
+    throw new Error("Email sending failed");
   }
 };
 
